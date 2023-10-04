7b85c98307f0b7f9b8d8743ddf50c1033708f674
Oluwasegun <oluyedis@gmail.com>
2023-10-04 07:01:22 +0100

    Made the expense screen responsive for landscape mode

--- lib/widgets/expenses_screen.dart
+++ lib/widgets/expenses_screen.dart
@@ -80,7 +80,12 @@ class _ExpensesState extends State<Expenses> {
   }
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
+    //This is used to get the width of the screen size and will be
+    //used to conditionally render the UI based on the orientation(portrait or landscape)
+    // N/B: Widgets get sized based on their size preferences & parent widget size constraints e.g the Column widgets height preference: takes as much as possible and it is important that is placed within a parent widget that restricts its height otherwise it will go outside of the screen boundary that's why it is wrapped within the scaffold widget most of the times because the scaffold widget by default constrains the height and width of whatever is placed within it to the maximum device height and width andthe contraints of the parent's widget overrides that of the child/children preferences  while width preference: takes as much as needed/defined by its children and so when we are sometimes confronted with issues like this wheremwe need to wrap an uncontrained widget within an unconstrained widget we used the Expanded widget because it helps to set height and width contraints
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text("No expenses found, start adding now!"),
     );
@@ -90,29 +95,48 @@ class _ExpensesState extends State<Expenses> {
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "BETASPEND",
-          style:
-              TextStyle(color: Colors.greenAccent, fontWeight: FontWeight.w700),
+        appBar: AppBar(
+          title: const Text(
+            "BETASPEND",
+            style: TextStyle(
+                color: Colors.greenAccent, fontWeight: FontWeight.w700),
+          ),
+          actions: [
+            IconButton(
+                onPressed: _addNewExpenseModal, icon: const Icon(Icons.add))
+          ],
         ),
-        actions: [
-          IconButton(
-              onPressed: _addNewExpenseModal, icon: const Icon(Icons.add))
-        ],
-      ),
-      body: Column(
-        children: [
-          Chart(
-            expenses: _registeredExpenses,
-          ),
-          // The reason for wrapping the ExpensesList inside the Expanded widget is
-          //because column expected a list but then it's gettin a list that contains another list
-          Expanded(
-            child: mainContent,
-          ),
-        ],
-      ),
-    );
+        body: width < 600
+            ? Column(
+                children: [
+                  Chart(
+                    expenses: _registeredExpenses,
+                  ),
+                  // The reason for wrapping the ExpensesList inside the Expanded widget is
+                  //because column expected a list but then it's gettin a list that contains another list
+                  Expanded(
+                    child: mainContent,
+                  ),
+                ],
+              )
+            : Row(
+                children: [
+                  // The reason this is wrapped inside expanded widget here is
+                  //because the Chart widget is configured to take as much space
+                  //as is available and by default the row widget works essentially
+                  //the same way and so to rectify any error this might cause we need
+                  //to wrap the child widget (Chart) inside expanded widget
+                  Expanded(
+                    child: Chart(
+                      expenses: _registeredExpenses,
+                    ),
+                  ),
+                  // The reason for wrapping the ExpensesList inside the Expanded widget is
+                  //because column expected a list but then it's gettin a list that contains another list
+                  Expanded(
+                    child: mainContent,
+                  ),
+                ],
+              ));
   }
 }
